39de469001aa4fcc6129530cd291c76c441b10e0
Matheus <matheusmorante002@gmail.com>
2025-01-20 22:51:07 -0300

    first commit

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import defaultPerfil from '../assets/images/defaultperfil.png';
+
+export default function Header() {
+    return (
+        <header>
+            Tarefando
+            <img src={defaultPerfil} alt="Perfil" />
+        </header>
+    )
+}
