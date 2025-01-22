cf202e7e2cf47d832177da3a923e3c92cb22d0f4
Matheus <matheusmorante002@gmail.com>
2025-01-21 22:48:37 -0300

    dd

--- server/server.js
+++ server/server.js
@@ -4,4 +4,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.arguments(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html' ));
+
+});
+
+app.listen(PORT, () => (console.log('Server rodando https://localhost:'+PORT)))
