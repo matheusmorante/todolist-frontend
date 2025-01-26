6e4597e63c13081891af16419b4d855e31eddb0d
Matheus <matheusmorante002@gmail.com>
2025-01-25 22:06:32 -0300

    dd

---
+++ client/src/components/Tasks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Tasks() {
+    return (
+        <section>
+            <h1>Tarefas</h1>
+            <div></div>
+            
+        </section>
+    )
+}
