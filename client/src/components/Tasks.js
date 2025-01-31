5c63d582a6c9325d32f0ac51521a8ab3f82bfd9d
Matheus Morante <matheusmorante002@gmail.com>
2025-01-31 16:58:48 -0300

    sdds222s

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState,  useEffect } from 'react';
+import Task from './Task';
 
 export default function Tasks() {
+    const [ tasks, setTasks ] = useState('');
     return (
         <section>
             <h1>Tarefas</h1>
             <div></div>
-            
+            <table>
+                { tasks.map(task => <Task />) }
+            </table>
         </section>
     )
 }
