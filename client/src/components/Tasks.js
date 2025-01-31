87cd84b09bfa7d0d52089272748d06b07bbb4e12
Matheus Morante <matheusmorante002@gmail.com>
2025-01-31 17:16:36 -0300

    sdds222s

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -2,13 +2,16 @@ import React, { useState,  useEffect } from 'react';
 import Task from './Task';
 
 export default function Tasks() {
-    const [ tasks, setTasks ] = useState('');
+    const [ tasks, setTasks ] = useState([]);
     return (
         <section>
             <h1>Tarefas</h1>
-            <div></div>
+            <div>
+                <input placeholder='Filtrar por...'/>
+                <i class="bi bi-sort-alpha-down"/>
+            </div>
             <table>
-                { tasks.map(task => <Task />) }
+                { tasks.map(task => <Task  task={ task }/>) }
             </table>
         </section>
     )
