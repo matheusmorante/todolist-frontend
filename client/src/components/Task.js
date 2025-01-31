f522fd1e33f834e53f8f72cc432b8a4ead004f54
Matheus Morante <matheusmorante002@gmail.com>
2025-01-31 16:36:36 -0300

    sdds222s

---
+++ client/src/components/Task.js
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+=======
+import React from 'react';
+
+export default function Task({ task }) {
+    return (
+        <tr>
+            <td>{ task.description } - { task.date }</td>
+            <td>
+                <i class="bi bi-check"></i>
+                <i class="bi bi-pencil"></i>
+                <i class="bi bi-trash3">2</i>
+            </td>
+
+        </tr>
+        
+    )
+}
+>>>>>>> 91a5659 (sdds222s)
